bde0898b38b95b175c461e25ef8ce77ee1619df9
Mantra <mantrakusumgar@outlook.com>
2023-12-14 13:58:41 +0000

    finish setup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true
+  },
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "VueNestedMultiselect",
+      fileName: "vue-nested-multiselect"
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue"
+        }
+      }
+    }
+  },
+})
